57c60405057e59ffc51410f527d415f2b68a603b
babamohammed2022 <kidmangtacool@gmail.com>
2025-08-06 09:10:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import ModFeedbackSection from "./pages/Feedback"; // <-- Add this import
+import ModFeedbackSection from "./components/ModFeedbackSection";
 
 const queryClient = new QueryClient();
 
